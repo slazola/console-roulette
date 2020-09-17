2357395d2468a4fb53173a50e76a1e8d1202053b
Singeni, Lazola <lsingeni@fnb.co.za>
2020-09-17 23:22:21 +0200

    roulette commit 2

--- src/com/lazola/roulette/Main.java
+++ src/com/lazola/roulette/Main.java
@@ -1,16 +1,25 @@
 package com.lazola.roulette;
 
 import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome. Please enter player list file");
+        System.out.println("Welcome. Please enter players list file with directory path.");
         Scanner scanner = new Scanner(new BufferedInputStream(System.in));
         if(scanner.hasNextLine()){
             String file = scanner.next();
-            System.out.println("File : " + file);
+            try {
+                String fileData = new String(Files.readAllBytes(Paths.get(file)));
+                System.out.println("Players : \n" + fileData);
+            }catch (IOException exc){
+                System.out.println(String.format("Error reading file : %s", exc.getMessage()));
+            }
+
         }
     }
 }
