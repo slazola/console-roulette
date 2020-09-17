16ef1989eabdb7914b46e1687d617fef1a341dfd
Singeni, Lazola <lsingeni@fnb.co.za>
2020-09-18 00:59:25 +0200

    roulette commit 5

--- src/com/lazola/roulette/Main.java
+++ src/com/lazola/roulette/Main.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +16,7 @@ public class Main {
         List<String> players = new ArrayList<>();
         List<String> bet  = new ArrayList<>();
         List<Double> amount = new ArrayList<>();
+        Random random = new Random();
 
         System.out.println("Welcome. Please enter players list file with directory path.");
         Scanner scanner = new Scanner(new BufferedInputStream(System.in));
@@ -29,7 +31,7 @@ public class Main {
 
         }
 
-        System.out.println("Enter number:\n 1 - place bet\n2 - close bets\n 0 - Exit game");
+        System.out.println("\nEnter number selection:\n 1 - place bet\n 2 - close bets\n 0 - Exit game");
         int selection = scanner.nextInt();
 
         do{
@@ -41,9 +43,23 @@ public class Main {
                     bet.add(scanner.next());
                     amount.add(Double.valueOf(scanner.next()));
 
-                    System.out.println("Players: " + players + " bet: " +  bet + "  Amount: " + amount);
                     break;
                 case 2:
+                    int num = random.nextInt(36 - 1 + 1) + 1;
+                    System.out.printf("Number: %d%n", num);
+                    System.out.println("Player    Bet   Outcome  Winnings");
+                    String fmt = "%s  %s  %s %.2f%n";
+                    for (int i = 0; i < players.size(); i++){
+                        if(bet.get(i).equalsIgnoreCase("ODD") && (num % 2 > 0)){
+                            System.out.printf(fmt,players.get(i),bet.get(i),"WIN", Double.valueOf(amount.get(i)) * 2);
+                        }else if(bet.get(i).equalsIgnoreCase("EVEN") && (num % 2 == 0)){
+                            System.out.printf(fmt,players.get(i),bet.get(i),"WIN", Double.valueOf(amount.get(i)) * 2);
+                        }else if(Integer.parseInt(bet.get(i)) == num){
+                            System.out.printf(fmt,players.get(i),bet.get(i),"WIN", Double.valueOf(amount.get(i)) * 36);
+                        }else{
+                            System.out.printf(fmt,players.get(i),bet.get(i),"LOSE", 0d);
+                        }
+                    }
                     break;
             }
             System.out.println("Enter number:\n 1 - place bet\n2 - close bets\n 0 - Exit game");
